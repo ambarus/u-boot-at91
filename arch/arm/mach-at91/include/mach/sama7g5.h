17bc5b4c988910f5ebdc8bb8e7f102381e73707f
Tudor Ambarus <tudor.ambarus@microchip.com>
2022-02-03 10:53:39 +0200

    board: atmel: sama7g5ek: Add NAND support
    Add NAND support on sama7g5ek. sama7g5ek does not populate a NAND
    flash, this is just an example on how to enable NAND on sama7g5
    without DM.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Tested-by: Mihai Sain <mihai.sain@microchip.com>

--- arch/arm/mach-at91/include/mach/sama7g5.h
+++ arch/arm/mach-at91/include/mach/sama7g5.h
@@ -12,6 +12,7 @@
 /*
  * Peripheral identifiers/interrupts.
  */
+#define ATMEL_ID_HSMC		21
 #define ATMEL_ID_FLEXCOM0	38
 #define ATMEL_ID_FLEXCOM1	39
 #define ATMEL_ID_FLEXCOM2	40
@@ -45,6 +46,8 @@
 #define ATMEL_BASE_WDTS		0xe001d180
 #define ATMEL_BASE_SCKCR	0xe001d050
 
+#define ATMEL_BASE_HSMC		0xe0808000
+
 #define ATMEL_BASE_SDMMC0	0xe1204000
 #define ATMEL_BASE_SDMMC1	0xe1208000
 
@@ -66,6 +69,31 @@
 #define ATMEL_PIO_PORTS		5
 
 #define ATMEL_BASE_PIT64BC	ATMEL_BASE_PIT64B0
+/*
+ * Internal Memory.
+ */
+#define ATMEL_BASE_ROM		0x00020000
+
+/*
+ * External memory
+ */
+#define ATMEL_BASE_CS0		0x40000000
+#define ATMEL_BASE_CS1		0x48000000
+#define ATMEL_BASE_CS2		0x50000000
+#define ATMEL_BASE_CS3		0x58000000
+
+/*
+ * Other misc definitions
+ */
+#define ATMEL_BASE_PMECC	(ATMEL_BASE_HSMC + 0x70)
+#define ATMEL_BASE_PMERRLOC	(ATMEL_BASE_HSMC + 0x500)
+#define ATMEL_BASE_SMC		(ATMEL_BASE_HSMC + 0x700)
+
+/*
+ * PMECC table in ROM
+ */
+#define ATMEL_PMECC_INDEX_OFFSET_512	0x0
+#define ATMEL_PMECC_INDEX_OFFSET_1024	0x8000
 
 #ifndef __ASSEMBLY__
 char *get_cpu_name(void);
--- board/atmel/sama7g5ek/sama7g5ek.c
+++ board/atmel/sama7g5ek/sama7g5ek.c
@@ -7,6 +7,7 @@
 #include <common.h>
 #include <debug_uart.h>
 #include <init.h>
+#include <clk.h>
 #include <asm/global_data.h>
 #include <asm/io.h>
 #include <asm/arch/at91_common.h>
@@ -14,6 +15,8 @@
 #include <asm/arch/clk.h>
 #include <asm/arch/gpio.h>
 #include <asm/arch/sama7g5.h>
+#include <asm/arch/sama5d2_smc.h>
+#include <dm/uclass.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -67,10 +70,52 @@ int misc_init_r(void)
 }
 #endif
 
+#ifdef CONFIG_NAND_ATMEL
+static void sama7g5ek_nand_hw_init(void)
+{
+	struct at91_smc *smc = (struct at91_smc *)ATMEL_BASE_SMC;
+
+	at91_periph_clk_enable(ATMEL_ID_HSMC);
+
+	writel(AT91_SMC_SETUP_NWE(4), &smc->cs[3].setup);
+	writel(AT91_SMC_PULSE_NWE(10) | AT91_SMC_PULSE_NCS_WR(20) |
+	       AT91_SMC_PULSE_NRD(10) | AT91_SMC_PULSE_NCS_RD(20),
+	       &smc->cs[3].pulse);
+	writel(AT91_SMC_CYCLE_NWE(20) | AT91_SMC_CYCLE_NRD(20),
+	       &smc->cs[3].cycle);
+	writel(AT91_SMC_TIMINGS_TCLR(4) | AT91_SMC_TIMINGS_TADL(15) |
+	       AT91_SMC_TIMINGS_TAR(5)  | AT91_SMC_TIMINGS_TRR(8)   |
+	       AT91_SMC_TIMINGS_TWB(8)  | AT91_SMC_TIMINGS_NFSEL(1),
+	       &smc->cs[3].timings);
+	writel(AT91_SMC_MODE_RM_NRD | AT91_SMC_MODE_WM_NWE |
+	       AT91_SMC_MODE_TDF | AT91_SMC_MODE_TDF_CYCLE(15),
+	       &smc->cs[3].mode);
+
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 9, 0);	/* NAND_D0 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 10, 0);	/* NAND_D1 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 11, 0);	/* NAND_D2 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTC, 21, 0);	/* NAND_D3 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTC, 22, 0);	/* NAND_D4 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTC, 23, 0);	/* NAND_D5 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTC, 24, 0);	/* NAND_D6 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 2, 0);	/* NAND_D7 */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 5, 0);	/* NAND_WE */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 6, 0);	/* NAND_OE */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 7, 0);	/* NAND_ALE */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 8, 0);	/* NAND_CLE */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 3, 0);	/* NAND_RDY */
+	atmel_pio4_set_d_periph(AT91_PIO_PORTD, 4, 0);	/* NAND_CS */
+}
+#endif
+
 int board_init(void)
 {
 	/* address of boot parameters */
 	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x100;
+
+#ifdef CONFIG_NAND_ATMEL
+	sama7g5ek_nand_hw_init();
+#endif
 
 	board_leds_init();
 
